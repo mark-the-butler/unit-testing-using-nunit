bc9ea428ed490110c451cc34ce1914dedd512ab7
Mark Butler <mysteryboy7300@gmail.com>
2018-09-11 07:08:39 -0400

    Unit testing with Nunit and DotNet Core

---
+++ PrimeService.Test/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    [TestFixture]
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void ReturnFalseGivenValueLessThan2(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.IsFalse(result, $"{value} should not be prime");
+        }
+    }
+}
---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if(candidate == 0 || candidate % 2 != 0)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Please create a test first");
+        }
+    }
+}
